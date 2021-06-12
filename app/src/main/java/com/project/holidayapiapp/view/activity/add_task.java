42ca230ae7049a62d47b930b215dd6089490d8d3
dbaskhara <dbaskhara@gmail.com>
2021-06-12 09:37:31 +0700

    input

--- app/src/main/java/com/project/holidayapiapp/view/activity/add_task.java
+++ app/src/main/java/com/project/holidayapiapp/view/activity/add_task.java
@@ -6,24 +6,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.project.holidayapiapp.R;
 
 public class add_task extends AppCompatActivity {
-
+    EditText editTextDate = findViewById(R.id.editTextDate);
+    EditText editTextTime = findViewById(R.id.editTextTime);
+    EditText editTextActivity = findViewById(R.id.editTextActivity);
+    EditText editTextDescription = findViewById(R.id.editTextDescription);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-        FloatingActionButton buttonAddTask = findViewById(R.id.floating_button_add);
-        buttonAddTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(add_task.this, add_task.class);
-                startActivity(intent);
-            }
-        });
+
+
+
     }
+    private void saveTask() {
+        final String sDate = editTextDate.getText().toString().trim();
+        final String sTime = editTextTime.getText().toString().trim();
+        final String sActivity = editTextActivity.getText().toString().trim();
+        final String sDescription = editTextDescription.getText().toString().trim();
+
+        if (sDate.isEmpty()) {
+            editTextDate.setError("Task required");
+            editTextDate.requestFocus();
+            return;
+        }
+
+        if (sTime.isEmpty()) {
+            editTextTime.setError("Desc required");
+            editTextTime.requestFocus();
+            return;
+        }
+
+        if (sActivity.isEmpty()) {
+            editTextActivity.setError("Finish by required");
+            editTextActivity.requestFocus();
+            return;
+        }
+
+        if (sDescription.isEmpty()) {
+            editTextDescription.setError("Finish by required");
+            editTextDescription.requestFocus();
+            return;
+        }
+
+    }
+
 }
--- app/src/main/java/com/project/holidayapiapp/view/fragment/DatabaseFragment.java
+++ app/src/main/java/com/project/holidayapiapp/view/fragment/DatabaseFragment.java
@@ -1,16 +1,20 @@
 package com.project.holidayapiapp.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.project.holidayapiapp.R;
+import com.project.holidayapiapp.view.activity.add_task;
 
 public class DatabaseFragment extends Fragment {
 
@@ -18,7 +22,17 @@ public class DatabaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_database, container, false);
+        View root = inflater.inflate(R.layout.fragment_database, null);
+        RecyclerView rv = (RecyclerView) root.findViewById(R.id.recyclerview_tasks);
+        FloatingActionButton buttonAddTask = root.findViewById(R.id.floating_button_add);
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(root.getContext(), add_task.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 
     @Override
