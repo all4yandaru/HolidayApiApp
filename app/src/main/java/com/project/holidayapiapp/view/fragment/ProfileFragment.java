fbff4aefdc0878d51f1a33ddd0fca9910ea1934b
dbaskhara <dbaskhara@gmail.com>
2021-06-13 10:31:46 +0700

    end game scenario

--- app/src/main/java/com/project/holidayapiapp/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/project/holidayapiapp/view/fragment/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.project.holidayapiapp.view.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,17 +11,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.project.holidayapiapp.R;
 
 public class ProfileFragment extends Fragment {
 
-
+    Button button , button1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        button = root.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.instagram.com/allyandaru25/?hl=id"));
+                startActivity(intent);
+            }
+        });
+        button1 = root.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.twitch.tv/dbaskhara"));
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 
     @Override
