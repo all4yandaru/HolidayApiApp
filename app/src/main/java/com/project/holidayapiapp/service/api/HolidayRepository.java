efb7cf4e9bf1c1a85e462ba9ea9d28dff6462753
Allyandaru <all4yandaru@gmail.com>
2023-01-01 12:31:26 +0700

    year update

--- app/src/main/java/com/project/holidayapiapp/service/api/HolidayRepository.java
+++ app/src/main/java/com/project/holidayapiapp/service/api/HolidayRepository.java
@@ -8,6 +8,6 @@ import retrofit2.http.Query;
 
 public interface HolidayRepository {
 
-    @GET("v1/holidays?pretty&key=b76340cd-bdb9-4c67-852a-850ef9139d52&country=ID&year=2020&")
+    @GET("v1/holidays?pretty&key=b76340cd-bdb9-4c67-852a-850ef9139d52&country=ID&year=2022&")
     Call<HolidayApiResponse> getAllHoliday(@Query("month") int code);
 }
